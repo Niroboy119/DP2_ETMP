f8596e1d68805825794b3107df18e368ab5cc155
firuz-beep <62094522+firuz-beep@users.noreply.github.com>
2021-04-27 15:57:10 +0500

    latest PHP files
    latest PHP files

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php include('include/header.php'); ?>
+
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1 style="text-align:center" class="display-4">Expert Management Training Portal (EMTP) </h1>
+      
+         <!-- <p class="lead text-primary ont-weight-bold"><?php echo $_SESSION['fullname']; ?></p> --> 
+      
+        <hr class="my-4">
+            <h4 style=" text-align: justify;"> 
+         <br>
+         
+            Expert.com is a local training provider which provides in-house training (or on
+              site/bespoke) for companies or employees within Sarawak. The
+              objectives of Training Expert.com is to provide hands-on solutions
+              through practical information sharing to help solve day to day
+              business challenges by developing human capitals that meets the
+              company’s needs.</h4>
+          </div>
+          
+     <br>
+     <br>
+          <div style="text-align: center;"><img src="images/training.jpg"
+
+              alt="traning" title="traning session" style="width: 840px; height: 560px;"></div>
+        </div>
+    </div>
+  </div>
+
+<?php include('include/footer.php'); ?>

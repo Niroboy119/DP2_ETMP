99d67ec06eabe82e3d0fb5a776ae33c7c11c07ec
saads2018 <71264405+saads2018@users.noreply.github.com>
2021-04-30 09:18:16 +0800

    Update php file
    The php file was updated to function with and be called from the javascript dealing with the validation of the form.

--- tRequest_action.php
+++ tRequest_action.php
@@ -1,19 +1,21 @@
 <?php 
 
-include "connection.php"
+include "connection.php";
 
-    $fullname = $_POST['fullname'];
+$output='';
+
+    $fullname = $_POST['fname'];
     $email = $_POST['email'];
     $tRequestDate = $_POST['dateTraining'];
     $trainingType = $_POST['training-type'];
     $oth_training_type = $_POST['othTrainings'];
     $numOfAttendees = $_POST['numOfParticipants'];
 
-    if ($oth_training_type == "") {
+    if ($oth_training_type == "Describe your desired training...") {
         $oth_training_type = "empty";
     }
 
-    $sql = "INSERT INTO tbl_users (fullname, email, date_for_training, training_type, other_training_type, num_of_attendees) 
+    $sql = "INSERT INTO tbl_client_requests (fullname, email, date_for_training, training_type, other_training_type, num_of_attendees) 
             VALUES('$fullname', 
                 '$email',
                 '$tRequestDate',
@@ -21,10 +23,19 @@ include "connection.php"
                 '$oth_training_type',
                 '$numOfAttendees')";
 
-    if (!$conn->query($sql)) {
-        die("error".mysqli_connect_error());
-    }else {
-        echo"client requested insert into database";
-    }
+   if(mysqli_query($conn, $sql)){
+          $output = array(
+              'status'        => 'success'
+          );
+      } else
+      {
+           $output = array(
+              'status'        => 'error'
+          );
+      }
+      
+          echo json_encode($output);
+    
 $conn->close();
+
 ?> 

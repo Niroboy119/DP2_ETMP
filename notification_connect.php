de73f9bc51ab7bd47f3621eb2a5ddcab6e27ec80
SaudSultan9 <101227897@students.swinburne.edu.my>
2021-05-07 22:36:16 +0800

    Update notification_connect.php

--- notification_connect.php
+++ notification_connect.php
@@ -1,4 +1,16 @@
-<?php 
-//connect.php;
-$connect = mysqli_connect("localhost", "root", "", "testing");
-?>
+<?php
+
+    $servername="localhost";
+    $username="id16670827_expertcom";
+    $password="1q2w3e4r5t!A";
+    $dbname="id16670827_expertcomm";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    // Check connection
+    if(!$conn){
+        die("Connection failed: ".mysqli_connect_error());
+    }
+
+ ?>

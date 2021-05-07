bd13919a8e0d9a32c8cd3a2481fc11072d34bef6
saads2018 <71264405+saads2018@users.noreply.github.com>
2021-05-07 22:39:02 +0800

    Update tRequest_action1.php

--- tRequest_action1.php
+++ tRequest_action1.php
@@ -2,6 +2,33 @@
 
 include "connection.php";
 
+$output='';
+
+    $fullname = $_POST['fname'];
+    $email = $_POST['email'];
+    $tRequestDate = $_POST['dateTraining'];
+    $trainingType = $_POST['training-type'];
+    $oth_training_type = $_POST['othTrainings'];
+    $numOfAttendees = $_POST['numOfParticipants'];
+    $billingEmail = $_POST['bEmail'];
+    $billingName = $_POST['bName'];
+    $address = $_POST['address'];
+    $city = $_POST['city'];
+    $state = $_POST['state'];
+    $zip = $_POST['zip'];
+    $cardName = $_POST['cardName'];
+    $cardNum = $_POST['cardNum'];
+    $expMonth = $_POST['expMonth'];
+    $expYear = $_POST['expYear'];
+    $cvv = $_POST['cvv'];
+    $payType="Online Payment";
+    
+    
+
+    
+    if ($oth_training_type == "Describe your desired training...") {
+        $oth_training_type = "empty";
+    }
 
     
 $conn->close();

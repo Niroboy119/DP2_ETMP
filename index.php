4be895995dba51912919eae67093d2d86d83b4ab
firuz-beep <62094522+firuz-beep@users.noreply.github.com>
2021-05-12 07:23:08 +0500

    Updated Homepage with more information
    Updated Homepage with more information

--- index.php
+++ index.php
@@ -10,12 +10,7 @@
             <h4 style=" text-align: justify;"> 
          <br>
          
-            Expert.com is a local training provider which provides in-house training (or on
-              site/bespoke) for companies or employees within Sarawak. The
-              objectives of Training Expert.com is to provide hands-on solutions
-              through practical information sharing to help solve day to day
-              business challenges by developing human capitals that meets the
-              company’s needs.</h4>
+            EMTP is the only online training portal you’ll need to nurture and care for your staff . Our courses project and change management, wellbeing, health and safety and more, so your team will have much-needed support and access to the latest training, wherever they are, whenever they need it. Our free online training portal covers important and relevant topics impacting the workplace today so your team can get started immediately.</h4>
         
           
      <br>
@@ -26,16 +21,59 @@
         </div>
     
       <hr>
-     <div>
-      <h1 style="text-align:center" class="display-4">About Us </h1>
+     <div class="container">
+      <h2 style="text-align:center" class="display-4">About Us </h2>
+      <br>
+     <h5 style=" text-align: justify;">
+         Expert.com is a local training provider which provides in-house training (or on
+site/bespoke) for companies or employees within Sarawak. The objectives of Training
+Expert.com is to provide hands-on solutions through practical information sharing to help solve
+day to day business challenges by developing human capitals that meets the company’s needs.
+The company established since 2001 and has trained more than 50,000 people to improving their
+work productivity, leadership & communication skills, language proficiency, sales, negotiation &
+presentation skills, workplace management and personal development. The company operates
+completely independent and is open for clients and public throughout the years.</h5> 
       </div>
       <hr>
-        <div>
-      <h1 style="text-align:center" class="display-4">News </h1>
+        <div class="container">
+      <h2 style="text-align:center" class="display-4">What We Offer </h2>
+      <br>
+     <h5 style=" text-align: justify;">
+         <ul>
+
+  <li>Courses are all available in one place on our easy-to-use online portal</li>
+<li>The information is simple to access. Log-in from anywhere and access all available training</li>
+<li>Create unique training regimes for staff, add new users and assign training</li>
+<li>Our packages don’t just deliver information, but provide practical tips and advice</li>
+<li>Quizzes, videos and tools make courses interactive, engaging and memorable</li>
+</ul>
+</h5>
       </div>
       <hr>
-      <div>
-      <h1 style="text-align:center" class="display-4">Events </h1>
+       <div class="container">
+      <h2 style="text-align:center" class="display-4">News </h2>
+      <br>
+     <h5 style=" text-align: justify;">
+         <dl>
+  <dt>27th April 2021 - More Training Types</dt>
+  
+  <dd>We have wonderful news for our clients and future clients. We have added new types of training which offer more versatility and flexibility which can be suited to the current shift of the world towards online work or working from home. Our new training types are sure to meet your expectations and even more. To know more, please sign up and get in touch with us</dd>
+    <br>
+  <dt>04th January 2021 - 4 Ways to Train Employees Effectively</dt>
+ 
+  <dd>1. Blend in-person training and online training.</dd>
+    <dd>2. Provide hands-on training.</dd>
+      <dd>3. Incorporate mobile training apps.</dd>
+        <dd>4. Allow employees to learn at their own pace.</dd>
+            <dd>More information at <a href="https://www.entrepreneur.com/article/253023">https://www.entrepreneur.com/article/253023</a></dd>
+          <br>
+        <dt>14th May 2020 - Training Update</dt>
+  
+  <dd>Due to the Coronavirus Pandemic, we will unfortunately not be able to do face to face training sessions until the Government allows us to do so. Till then it will be online via Zoom. We would like to apologize for the inconvenience.</dd>
+    <br>
+        
+</dl>
+</h5>
       </div>
   </div>
   </div>
@@ -44,4 +82,6 @@
      <br>
      <br>
      </div>
+     
+     
 <?php include('include/footer.php'); ?>

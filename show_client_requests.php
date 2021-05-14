227389300db8661a97ff14724ffca998b92ead12
Nirodha <niroman119@gmail.com>
2021-05-15 02:11:57 +0800

    load client requests based on current logged in user

--- show_client_requests.php
+++ show_client_requests.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include "connection.php";
-$sql = "SELECT * FROM tbl_client_requests WHERE fullname = '{$_SESSION['fullname']}' ";
+$sql = "SELECT * FROM tbl_client_requests WHERE userId = '{$_SESSION['user_id']}' ";
 
 $result = $conn->query($sql);
 	if ($result->num_rows > 0) {
@@ -15,6 +15,7 @@ $result = $conn->query($sql);
 			<td><?=$row['training_type'];?></td>
 			<td><?=$row['num_of_attendees'];?></td>
 			<td><?=$row['paymentType'];?></td>
+			<td><button class="btn btn-primary btn-lg btn-block" id="<?=$row['id'];?>">Select</button></td>
 		</tr>
 <?php	
 	}

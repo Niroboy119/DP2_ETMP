d5d77a2e66b5692c75a4e7f35a51fc50d105de00
firuz-beep <62094522+firuz-beep@users.noreply.github.com>
2021-05-16 06:52:55 +0500

    Updated Contact Us Form with header and footer
    Updated Contact Us Form with header and footer

--- form.php
+++ form.php
@@ -20,45 +20,46 @@
          <img class="mb-2" src="images/logo.jpg" alt="" width="225" height="100">
       </div>
       <h1 class="text-center">Contact Us Form</h1>
-      <form action="form_action.php" method="POST">
+  <form name="contact-form" action="" method="post" id="contact-form">
+      <div class="form-group">
          <br>
          <br>
-         <div class="mb-3">
-            <label for="fullname">Full Name</label>
-            <input type="text" class="form-control" id="fullname" name="fullname" maxlength="50"
+          <div class="form-group" class="mb-3">
+            <label for="name">Full Name</label>
+            <input type="text" class="form-control" id="your_name" name="your_name" maxlength="50"
                placeholder="Enter full name">
             <div id="fullname_error_message" class="text-danger"></div>
          </div>
-         <div class="mb-3">
+          <div class="form-group" class="mb-3">
             <label for="email">Email</label>
-            <input type="text" class="form-control" id="email" name="email" maxlength="50"
+            <input type="text" class="form-control" id="your_email" name="your_email" maxlength="50"
                placeholder="Enter email">
             <div id="fullname_error_message" class="text-danger"></div>
          </div>
-         <div class="mb-3">
+          <div class="form-group" class="mb-3">
             <label for="fullname">Phone Number</label>
-            <input type="text" class="form-control" id="phone" name="phone" maxlength="50"
+            <input type="text" class="form-control" id="your_phone" name="your_phone" maxlength="50"
                placeholder="Enter Phone Number">
             <div id="fullname_error_message" class="text-danger"></div>
          </div>
-         <div class="mb-3">
+          <div class="form-group" class="mb-3">
             <label>Request Phone Call:</label>
-            <select name="request" id="request" class="custom-select">
+            <select name="your_phonecall" id="your_phonecall" class="custom-select">
                <option value="" hidden>Request Phone Call</option>
                <option>Yes</option>
                <option>No</option>
             </select>
             <div id="gender_error_message" class="text-danger"></div>
          </div>
-         <div class="mb-3">
+          <div class="form-group" class="mb-3">
             <label for="email">Website</label>
-            <input type="text" class="form-control" id="website" name="website" maxlength="50"
+            <input type="text" class="form-control" id="your_website" name="your_website" maxlength="50"
                placeholder="Enter Website">
             <div id="fullname_error_message" class="text-danger"></div>
          </div>
-         <div class="mb-3">
+          <div class="form-group" class="mb-3">
             <label>Priority</label>
-            <select name="priority" id="priority" class="custom-select">
+            <select name="your_priority" id="your_priority" class="custom-select">
                <option value="" hidden>Priority</option>
                <option>Low</option>
                <option>Normal</option>
@@ -67,9 +68,9 @@
             </select>
             <div id="gender_error_message" class="text-danger"></div>
          </div>
-         <div class="mb-3">
+         <div class="form-group" class="mb-3">
             <label>Regarding</label>
-            <select name="type" id="type" class="custom-select">
+            <select name="your_type" id="your_type" class="custom-select">
                <option value="" hidden>Type</option>
                <option>Training Request</option>
                <option>Information</option>
@@ -77,42 +78,62 @@
             </select>
             <div id="gender_error_message" class="text-danger"></div>
          </div>
-         <div id="othT-mb3" class="mb-3">
-            <label for="message">Message</label>
-            <textarea class="form-control" id="description" name="description"  rows="5" cols="20">Send us your message</textarea>
+         <div class="form-group" id="othT-mb3" class="mb-3">
+            <label for="comments">Message</label>
+            <textarea class="form-control" id="comments" name="comments"  rows="5" cols="20">Send us your message</textarea>
             <div id="otherTraining_error_message" class="text-danger"></div>
          </div>
          <br>
          <class="mb-4">
-         <input type="hidden" name="action" id="action" value="submit">
+         <input type="hidden" name="submit" id="submit_form" value="Submit">
          <button class="btn btn-primary btn-lg btn-block" type="submit">Submit</button>
          <hr
          <input type="hidden" name="action" id="action" value="reset">
          <button class="btn btn-primary btn-lg btn-block" type="reset">Reset</button>
       </form>
-	  <script>
-	         $.ajax({
-                        type: "POST",
-                        data: { 'fullname': $("#fullname").val(), 'email': $("#email").val(), 'phone': $("#phone").val(), 'request': $("#request").val(), 'website': $("#website").val()  ,'priority': $("#priority").val(), 'type': $("#type").val(), 'description': $("#description").val() },
-                        url: "form_action.php",
-                        dataType: "json",
-                        success: function (data) {
-                            if (data.status == 'success') {
-                                $("#alert_sucess_message").show();
-                                $("#alert_error_message").hide();
-                                $('#training_request_form')[0].reset();
-                            } else if (data.status == 'error') {
-                               alert("Oops! Something went wrong.");
-                            }
-                        },
-                        error: function () {
-                            alert("Oops! Something went wrong.");
-                        }
-                    });
-					</script>
-   </body>
+<div class="response_msg"></div>
+</div>
+</div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+$("#contact-form").on("submit",function(e){
+e.preventDefault();
+if($("#contact-form [name='your_name']").val() === '')
+{
+$("#contact-form [name='your_name']").css("border","1px solid red");
+}
+else if ($("#contact-form [name='your_email']").val() === '')
+{
+$("#contact-form [name='your_email']").css("border","1px solid red");
+}
+else
+{
+$("#loading-img").css("display","block");
+var sendData = $( this ).serialize();
+$.ajax({
+type: "POST",
+url: "form_action.php",
+data: sendData,
+success: function(data){
+$("#loading-img").css("display","none");
+$(".response_msg").text(data);
+$(".response_msg").slideDown().fadeOut(3000);
+$("#contact-form").find("input[type=text], input[type=email], textarea").val("");
+}
+});
+}
+});
+$("#contact-form input").blur(function(){
+var checkValue = $(this).val();
+if(checkValue != '')
+{
+$(this).css("border","1px solid #eeeeee");
+}
+});
+});
+</script>
+</body>
 </html>
-<br>
-<br>
-<br>
 <?php include('include/footer.php'); ?>

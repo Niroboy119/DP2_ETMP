783c86abc1a45896dd13e18e9ad2fd321f9f0ae0
saads2018 <71264405+saads2018@users.noreply.github.com>
2021-05-17 22:13:27 +0800

    Update admin_action.php
    Add Requests Made, Requests Pending, Requests Accepted and Total Revenue in admin reports

--- admin/admin_action.php
+++ admin/admin_action.php
@@ -10,22 +10,40 @@ include "../connection.php";
       $sqlInactiveUser = "SELECT count(id) FROM tbl_users WHERE status = 'Inactive'";
       $sqlActiveUser = "SELECT count(id) FROM tbl_users WHERE status = 'Active'";
       $sqlAdmin = "SELECT count(id) FROM tbl_admin";
+      $sqlRequests = "SELECT count(id) FROM tbl_client_requests";
+      $sqlRequestsPending = "SELECT count(id) FROM tbl_client_requests WHERE approval = 'Pending'";
+      $sqlRequestsAccepted = "SELECT count(id) FROM tbl_client_requests WHERE approval = 'Approved'";
+      $sqlRevenue = "SELECT SUM(paymentAmount) FROM tbl_client_requests WHERE approval = 'Approved'";
+
 
       $userResult = mysqli_query($conn, $sqlUser);
       $inactiveUserResult = mysqli_query($conn, $sqlInactiveUser);    
       $activeUserResult = mysqli_query($conn, $sqlActiveUser);    
       $adminResult = mysqli_query($conn, $sqlAdmin);
-
+      $requestsResult = mysqli_query($conn, $sqlRequests);
+      $requestsPendingResult = mysqli_query($conn, $sqlRequestsPending);
+      $requestsAcceptedResult = mysqli_query($conn, $sqlRequestsAccepted);
+      $revenueResult = mysqli_query($conn, $sqlRevenue);
+      
       $row1 = mysqli_fetch_row($userResult);
       $row2 = mysqli_fetch_row($inactiveUserResult);
       $row3 = mysqli_fetch_row($activeUserResult);
       $row4 = mysqli_fetch_row($adminResult);
-
+      $row5 = mysqli_fetch_row($requestsResult);
+      $row6 = mysqli_fetch_row($requestsPendingResult);
+      $row7 = mysqli_fetch_row($requestsAcceptedResult);
+      $row8 = mysqli_fetch_row($revenueResult);
+      
       $output=array(
         'total_users'               => $row1[0],
         'total_inactive_users'      => $row2[0],
         'total_active_users'        => $row3[0],
         'total_admins'              => $row4[0],
+        'total_requests'            => $row5[0],
+        'requests_pending'          => $row6[0],
+        'requests_accepted'         => $row7[0],
+        'total_revenue'             => $row8[0],
+        
       );
       echo json_encode($output);
     }
@@ -49,7 +67,6 @@ include "../connection.php";
     }
 
     if($_POST["action"] == "gender_info"){
-
       $sqlTotalMale = "SELECT count(gender) FROM tbl_users WHERE gender = 'Male'";
       $sqlTotalFemale = "SELECT count(gender) FROM tbl_users WHERE gender = 'Female'";
 

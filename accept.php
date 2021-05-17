a776bafe5e15a9e858760067896cfb7d7a5921c3
SaudSultan9 <101227897@students.swinburne.edu.my>
2021-05-17 22:45:26 +0800

    Update accept.php

--- accept.php
+++ accept.php
@@ -4,21 +4,22 @@ error_reporting(0);
 
 $id = $_GET['id']; // get id through query string
 
-//$query = "DELETE FROM tbl_client_requests WHERE id='$id'";
-
-$query = "INSERT INTO tbl_accepted_requests
-SELECT * FROM tbl_client_requests
+$query = "UPDATE tbl_client_requests
+SET approval = 'Approved'
 WHERE id='$id'";
+
+//$query = "INSERT INTO tbl_accepted_requests
+//SELECT * FROM tbl_client_requests
+//WHERE id='$id'";
 
 $data=mysqli_query($conn,$query); 
 
-if($data){
-    
-    echo '<script>alert("Request Accepted")</script>';
-}else{
-    echo '<script>alert("Request not placed.")</script>';
 
-}
+
+
+header("Location: profile.php");
+    exit;
+
 
 
 ?>

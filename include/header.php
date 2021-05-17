a49c75b7fc1ff7a4b0525196b587ddb369afb95d
saads2018 <71264405+saads2018@users.noreply.github.com>
2021-05-17 22:00:08 +0800

    Update header.php

--- include/header.php
+++ include/header.php
@@ -20,11 +20,12 @@
 
     <title>EMTP - Expert Management Training Portal</title>
     
-     <!-- Dropdown 
+    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" type ="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">-->
+   <link rel="stylesheet" type ="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     
+
 
     <!-- Custom CSS -->
     <link rel="stylesheet" type="text/css" href="css/style.css">
@@ -37,11 +38,23 @@
 
     <!-- Favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="images/favicon.jpg">
-    
+
+<style type="text/css">
+
+.size {
+    font-size:16px;
+}
+
+.dropdown-toggle{
+   color:grey;
+
+    }
+
+</style>
 
   </head>
 
-    <body class="bg-light">
+    <body class="size">
       
       <!-- Navigation -->
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
@@ -54,6 +67,14 @@
               
             <img src="images/logo-2.jpg" alt="50">
           </a>
+          
+          
+           
+         
+           <h3 style="text-align:center;color:white;"><b> Welcome <?php echo $_SESSION['fullname']; ?> </b></h3>
+         
+        
+          
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -66,22 +87,66 @@
                   <a class="nav-link" href="profile.php"><span class="fas fa-user"></span> Profile</a>
               </li>
                <li class="nav-item">
-                  <a class="nav-link" href="requestform.html"><span class="fas fa-book"></span> Booking</a>
+                  <a class="nav-link" href="requestform.php"><span class="fas fa-book"></span> Booking</a>
               </li>
                    <li class="nav-item">
                   <a class="nav-link" href="chatroom.php"><span class="fas fa-comments"></span> Chat With Us</a>
               </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="form.html"><span class="fas fa-address-book"></span> Contact Us</a>
+                  <a class="nav-link" href="form.php"><span class="fas fa-address-book"></span> Contact Us</a>
               </li>
               <li class="nav-item">
                   <a class="nav-link" href="logout.php"><span class="fas fa-sign-out-alt"></span> Logout</a>
               </li>
               <ul class="nav navbar-nav navbar-right">
+      <!--<li>-->
+      <!-- <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="label label-pill label-danger count" style="border-radius:10px;"></span></a>-->
+      <!-- <ul class="dropdown-menu"></ul>-->
+      <!--</li>-->
       <li class="dropdown">
        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="label label-pill label-danger count" style="border-radius:10px;"></span> <span class="glyphicon glyphicon-envelope" style="font-size:18px;"></span></a>
        <ul class="dropdown-menu"></ul>
+      </li>
+            </ul>
             </ul>
           </div>
         </div>
       </nav>
+      
+      
+<script>
+    $(document).ready(function(){
+     
+     function load_unseen_notification(view = '')
+     {
+      $.ajax({
+       url:"notification_fetch.php",
+       method:"POST",
+       data:{view:view},
+       dataType:"json",
+       success:function(data)
+       {
+        $('.dropdown-menu').html(data.notification);
+        if(data.unseen_notification > 0)
+        {
+         $('.count').html(data.unseen_notification);
+        }
+       }
+      });
+     }
+     
+     load_unseen_notification();
+     
+    
+     
+      $(document).on('click', '.dropdown-toggle', function(){
+      $('.count').html('');
+      load_unseen_notification('yes');
+     });
+     
+     setInterval(function(){ 
+      load_unseen_notification();; 
+     }, 5000);
+     
+    });
+</script>
